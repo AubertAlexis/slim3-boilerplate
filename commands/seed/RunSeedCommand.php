ffcd21f1f3513c92fba5b093ecbd12b464183799
Thibaud Chauviere <thibaud.chauviere@gmail.com>
2019-05-06 11:05:06 +0200

    // RunSeed command is now working in manager

--- commands/seed/RunSeedCommand.php
+++ commands/seed/RunSeedCommand.php
@@ -11,18 +11,37 @@ use Symfony\Component\Console\Output\OutputInterface;
 class RunSeedCommand extends Command
 {
     protected $commandName = 'seed:run';
-    protected $commandDescription = "Run your seed files";
+    protected $commandDescription = "Run your seeds files (without --seed, it will run all of them at once)";
+
+    protected $commandOptionSeedName = "seed";
+    protected $commandOptionSeedNameShortcut = "s";
+    protected $commandOptionSeedNameDescription = "Specify one Seed name that will be the only one to be executed";
 
     protected function configure()
     {
         $this
             ->setName($this->commandName)
+            ->addOption(
+                $this->commandOptionSeedName,
+                $this->commandOptionSeedNameShortcut,
+                InputOption::VALUE_REQUIRED,
+                $this->commandOptionSeedNameDescription,
+                null
+            )
             ->setDescription($this->commandDescription);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $commandResult = 'None';
+        $seedName = $input->getOption($this->commandOptionSeedName);
+
+        $runSeedCmd = "php vendor/bin/phinx seed:run -c db/config/config.php -e db";
+
+        if ($seedName) {
+            $runSeedCmd .= " -s $seedName";
+        }
+
+        $commandResult = shell_exec($runSeedCmd);
 
         $output->writeln($commandResult);
     }
